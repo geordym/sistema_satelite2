2eff7f6f56491e3989e5bfd9dca2f65ebedf29fd
DESKTOP-QIPD8KD\Hp <ggeordymm@gmail.com>
2023-11-10 16:20:10 -0500

    Redirecting to home

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // Redirige a la ruta '/home'
+    return redirect('/home');
 });
 
 Auth::routes();
