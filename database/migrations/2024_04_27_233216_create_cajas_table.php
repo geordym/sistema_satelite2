ea1c2e1cac716c940ff35cab6bcfea7debc9ca04
geordym <ggeordymm@gmail.com>
2024-05-06 09:30:25 -0500

    Changed migration

--- database/migrations/2024_04_27_233216_create_cajas_table.php
+++ database/migrations/2024_04_27_233216_create_cajas_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('cajas', function (Blueprint $table) {
             $table->id();
-            $table->string('ip');
+            $table->string('mac');
             $table->string('nombre');
             $table->enum('estado', ['activado', 'desactivado'])->default('desactivado');
             $table->timestamps();
