5e404e8d2334d7b2fc9fb02cdc0872f27a736307
Geordy <ggeordymm@gmail.com>
2025-03-24 14:16:09 -0500

    Modificado web.php

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::match(['get', 'head'], '/', function () {
-    // Redirige a la ruta '/home'
+Route::get('/', function () {
     return redirect('/home');
 });
+
 
 Auth::routes();
 
